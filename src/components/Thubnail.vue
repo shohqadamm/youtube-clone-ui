f3321169f8adcd713981a8e027a561cf227a65e6
Shohqadamm <ergashevshohqadam@gmail.com>
2022-01-20 22:58:23 -0800

    data json

--- src/components/Thubnail.vue
+++ src/components/Thubnail.vue
@@ -1,12 +1,12 @@
 <template>
   <v-container>
       <v-row class="mx-5" no-gutters>
-          <v-col class="px-1" cols="3" v-for="(movie, index) in movies" :key="index">
+          <v-col class="px-1" cols="3" v-for="(video, index) in videos" :key="index">
               <v-card max-width="350" color="transparent" outlined>
-                  <v-img class="white--text align end" :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"> </v-img>
+                  <v-img class="white--text align end" :src="`https://image.tmdb.org/t/p/w500/${video.poster_path}`"> </v-img>
                   <v-card-text class="text--primary">
                       <div class="font-weight-bold">
-{{movie.title.slice(0,25)}} 
+{{video.title}} 
                       </div>
 
                        <div class="text--secondary">
@@ -24,32 +24,13 @@
 </template>
 
 <script>
-import axios from 'axios'
+import myvideos from '../assets/data.json'
 export default {
-data: function(){
-return { movies:[],}
-},
-  async fetch(){
-    if(this.searchInput === ''){
-      await this.getmovies()
-      return
-    }
-  },
-    fetchDelay: 1000,
-  methods: {
-    async getmovies(){
-      const data = axios.get(
-        'https://api.themoviedb.org/3/movie/now_playing?api_key=b8a7c2b3c1891a4f2e697f3983ed0d2e'
-      )
-      const result = await data
-      result.data.results.forEach((movie) => {
-        this.movies.push(movie)
-      });
-      console.log('asdf');
-    },
-    get(){
-      console.log(movies)
-    }  },
+data: ()=>({
+ 
+   videos: myvideos
+ 
+}),
 }
 </script>
 
